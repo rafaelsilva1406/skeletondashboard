e3aaefe96b8dca0192a28323dcad65a8bb553350
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2016-02-04 18:04:09 +0000

    Update Frontend Resources

--- app/main.js
+++ app/main.js
@@ -1,7 +1,15 @@
-require('angular')
-
-var app = angular.module('app', []);
-
+    var angular = require('angular'),
+    uiRouter = require('angular-ui-router'),
+    app = angular.module('app', [uiRouter]);
+app.config(function($stateProvider,$urlRouterProvider){
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+        .state('index',{
+           url:'',
+           templateUrl:'partials/test.html',
+           controller:'MainController' 
+        });
+});
 app.controller('MainController', function($scope) {
-    $scope.message = 'Live change on reload!'
+    $scope.message = 'Live change on reload view!';
 });
